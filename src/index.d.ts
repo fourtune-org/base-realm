6fbbaeb14665c5d6362c8abf3a6a2b811d5f580b
eXory2024 <exory@anio.software>
2024-09-16 15:36:19 +0200

    Fix index.d.ts.

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,8 +3,17 @@ interface LoadRealmDependenciesResult {
 	getDependencyVersion(name : string) : string|null
 }
 
+interface Dependency {
+	version : string
+	import_code : string
+}
+
+interface DependencyMap {
+	[name: string] : Dependency
+}
+
 export function findProjectRootFromDirectory(start_dir : string) : Promise<string|false>
-export function installRealmDependencies(project_root : string) : void
+export function installRealmDependencies(project_root : string, dependencies : DependencyMap) : void
 export function loadRealmDependencies(project_root : string) : Promise<LoadRealmDependenciesResult>
 
 declare const _default: {
